279ba66d1c6cbd0a10d6a9b9dc8b7c4bc2e6de31
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-08-04 21:12:22 +0300

    start 2 video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', onPlay);
+    
+        function onPlay (data) {
+    // data is an object containing properties specific to that event
+};
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
