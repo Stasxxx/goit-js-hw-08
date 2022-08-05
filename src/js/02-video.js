e3889420a33f1d6734f3649703bd1a9bae0b1575
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-08-05 19:04:48 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,34 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const sevedTime = localStorage.getItem("videoplayer-current-time");
+let parsedTime;
 
-player.on('play', onPlay);
+if (sevedTime) {
+    parsedTime = JSON.parse(sevedTime);
+} else {
+    parsedTime = 0;
+}
+
+player.on('timeupdate', onPlay);
     
-        function onPlay (data) {
-    // data is an object containing properties specific to that event
+function onPlay(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+    
 };
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+
+player.setCurrentTime(parsedTime.seconds)
+    .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
