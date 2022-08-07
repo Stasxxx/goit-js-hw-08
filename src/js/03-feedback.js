5c294bf8936781e56d91ffc21be10580a9996303
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-08-07 20:34:54 +0300

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,15 +1,16 @@
-const LOCALSTORAGE_KEY = 'feedback-form-state';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 let formData = {};
 
 const refs = {
     form: document.querySelector(".feedback-form"),
     input: document.querySelector('.feedback-form input'),
     textarea: document.querySelector('.feedback-form textarea'),
-    button: document.querySelector('.feedback-form button'),
 }
+ 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.form.addEventListener('input', onTextareaInput);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 populateTextarea()
 
@@ -18,42 +19,30 @@ function onFormSubmit(evt) {
     const elements = evt.currentTarget.elements;
     const email = elements.email.value;
     const message = elements.message.value;
-    
-    formData = {
-        email: email,
-        message: message,
-    }
-    console.log(formData)
+    console.log(message)
+    if (message === "" || email === "") {
+       alert("Заповніть пусті поля")
+    }else {
     evt.currentTarget.reset()
     localStorage.removeItem(LOCALSTORAGE_KEY)
+    formData = {}
+    console.log(formData)
+    }
 }
 
 function onTextareaInput(evt) {
-    const value = evt.currentTarget.elements;
-    const email = value.email.value;
-    const message = value.message.value;
-    formData = {
-        email: email,
-        message: message,
-    }
-
-    
-// console.log(Object.value);
-    // console.log(evt.target.name);
-    // if (evt.target.name === 'email') {
-    //     formData.email.push(evt.target.value);
-
-    // }
-    
+    formData[evt.target.name] = evt.target.value;
+   
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
 }
 
-
 function populateTextarea() {
-    const saveValue = localStorage.getItem(LOCALSTORAGE_KEY);
+    const saveValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    
 
     if (saveValue) {
-    refs.textarea.value = saveValue;
+        refs.input.value = saveValue.email || "";
+        refs.textarea.value = saveValue.message || "";
   }
 }
 
